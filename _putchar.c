49a1aaf2f98c39005e18aa60afc402aaf85eb98e
Uriel-229 <urielgodonou7@gmail.com>
2022-11-21 03:11:37 -0800

    Printf

---
+++ _putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar -  writes thee character c to print stdout
+ * @c: char to be printed
+ * Return: 1 on success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

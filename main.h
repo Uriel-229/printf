2c92fe29df2f1b1b764766c4270162055da67ecd
Symplice BONI <bonisimplice@gmail.com>
2022-11-22 04:00:24 +0000

    add somes utils lib in main.h

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 

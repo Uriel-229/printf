4b852354cf7123c7a8ff35a5fb6578801e1b8bb1
Symplice BONI <bonisimplice@gmail.com>
2022-11-22 04:09:56 +0000

    fix betty on _printf.c file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: input format
+ * Return: the number of characters printed excluding the null byte
+ */
+
+int _printf(const char *format, ...)
+{
+
+
+}
